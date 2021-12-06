c583849163686417dbc711f334e9a0fffe8178bb
chernyaevpavel <pawliklist@yandex.ru>
2021-12-06 20:07:01 +0400

    Initial Commit

---
+++ GBShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShop
+//
+//  Created by Павел Черняев on 06.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
